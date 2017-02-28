88646bc16dc3c141509e5edaa2666043b3a08758
Nick Fitzgerald <fitzgen@gmail.com>
2017-02-28 11:35:03 -0800

    Add snappy

---
+++ build.rs
@@ -0,0 +1,39 @@
+use std::env;
+use std::path::PathBuf;
+use std::process::Command;
+
+fn main() {
+    println!("rerun-if-changed=build.rs");
+    println!("rerun-if-changed=snappy");
+
+    let root_dir = PathBuf::from(
+        env::var("CARGO_MANIFEST_DIR")
+            .expect("should have CARGO_MANIFEST_DIR var"));
+    assert!(root_dir.is_dir());
+
+    let snappy_dir = {
+        let mut dir = root_dir.clone();
+        dir.push("snappy");
+        dir
+    };
+    assert!(snappy_dir.is_dir());
+
+    let status = Command::new("./autogen.sh")
+        .current_dir(&snappy_dir)
+        .status()
+        .expect("should spawn autogen.sh ok");
+    assert!(status.success(),
+            "autogen.sh should finish ok");
+
+    let status = Command::new("make")
+        .args(&["snappy.la"])
+        .current_dir(&snappy_dir)
+        .status()
+        .expect("should spawn make ok");
+    assert!(status.success(),
+            "make should finish ok");
+
+    println!("rustc-link-lib=static=bz2");
+    println!("rustc-link-search=static={}",
+             snappy_dir.to_str().expect("dir should be utf8"));
+}
