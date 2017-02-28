821ea4b2abf992a6716969a712e442c84d359a65
Nick Fitzgerald <fitzgen@gmail.com>
2017-02-28 11:42:10 -0800

    Build snappy in build.rs

--- build.rs
+++ build.rs
@@ -25,15 +25,30 @@ fn main() {
     assert!(status.success(),
             "autogen.sh should finish ok");
 
+    let status = Command::new("./configure")
+        .current_dir(&snappy_dir)
+        .status()
+        .expect("should spawn configure ok");
+    assert!(status.success(),
+            "configure should finish ok");
+
     let status = Command::new("make")
-        .args(&["snappy.la"])
+        .args(&["-f", "MyMakefile"])
         .current_dir(&snappy_dir)
         .status()
         .expect("should spawn make ok");
     assert!(status.success(),
             "make should finish ok");
 
-    println!("rustc-link-lib=static=bz2");
-    println!("rustc-link-search=static={}",
+    let libsnappy = {
+        let mut lib = snappy_dir.clone();
+        lib.push("libsnappy.a");
+        lib
+    };
+    assert!(libsnappy.is_file());
+
+    println!("cargo:rustc-link-lib=static=snappy");
+    println!("cargo:rustc-link-lib=c++abi");
+    println!("cargo:rustc-link-search={}",
              snappy_dir.to_str().expect("dir should be utf8"));
 }
